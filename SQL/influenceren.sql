15f7f156989548346b26b18c70425cb7cb461cca
Jonas Offersen <72074478+zelanard@users.noreply.github.com>
2023-08-21 14:35:22 +0200

    SQL DLL Update

--- SQL/influenceren.sql
+++ SQL/influenceren.sql
@@ -71,11 +71,21 @@ WHERE Brands.t_brand_id = (
 		)
 	);
 
+--11.2 I figured the where clause might be hard to read, so i treid with join. I still prefer my first option...
+SELECT DISTINCT Brands.t_name
+FROM Brands
+jOIN Collaborations
+	ON Brands.t_brand_id = Collaborations.t_brand_id
+WHERE t_duration = (
+	SELECT MAX(t_duration) 
+	FROM Collaborations
+);
+
 --12
 SELECT Influencers.t_name, COUNT(Collaborations.t_influencer_id) AS "Count"
 FROM Influencers, Collaborations
 WHERE Collaborations.t_influencer_id = Influencers.t_influencer_id
-GROUP BY Influencers.t_name
+GROUP BY Influencers.t_name;
 
 --13
 SELECT DISTINCT Brands.t_name
@@ -96,3 +106,15 @@ LEFT JOIN SocialMediaPlatforms
 GROUP BY SocialMediaPlatforms.t_name;
 
 --15
+SELECT Influencers.t_name, COUNT(Collaborations.t_brand_id) AS 'Mush'
+FROM Collaborations
+JOIN Influencers
+	ON Influencers.t_influencer_id = Collaborations.t_influencer_id
+WHERE (
+	SELECT COUNT(*) 
+	FROM Collaborations
+	WHERE Collaborations.t_influencer_id = Influencers.t_influencer_id
+	) > 1
+GROUP BY Influencers.t_name;
+
+--16
